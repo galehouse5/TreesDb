a5f3b87bef372841a150076713c6fbfc189e100c
galehouse5 <galehouse.5@gmail.com>
2011-01-04 02:24:06 +0000

    Removed legacy css and javascript files.
    git-svn-id: https://subversion.assembla.com/svn/treesdb/branches/DialoglessImport@137 d913c9e1-59cf-094f-9d4b-ab1779aea92f

--- TMD/Scripts/Account.js
+++
@@ -1,9 +0,0 @@
-﻿$(function () {
-    $('input[type=submit]').button();
-    $('a.button').button();
-    if ($('input[type=text].input-validation-error, input[type=password].input-validation-error').length > 0) {
-        $('input[type=text].input-validation-error, input[type=password].input-validation-error').first().focus();
-    } else {
-        $('input[type=text]').first().focus();
-    }
-});
