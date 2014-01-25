a8a827896fffc0d6f8fd35368db93743d3aace73
galehouse5 <galehouse.5@gmail.com>
2014-01-25 22:15:34 +0000

    Create placeholder HTML markup for import page.
    git-svn-id: https://subversion.assembla.com/svn/treesdb/branches/ExcelImport@305 d913c9e1-59cf-094f-9d4b-ab1779aea92f

--- TMD/Controllers/ImportController.cs
+++ TMD/Controllers/ImportController.cs
@@ -1,4 +1,7 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using TMD.Models.Import;
 
 namespace TMD.Controllers
 {
@@ -8,6 +11,15 @@ namespace TMD.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost, Route("import")]
+        public ActionResult Index(ImportModel model)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            throw new NotImplementedException();
         }
     }
 }
---
+++ TMD/Models/Import/ImportModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace TMD.Models.Import
+{
+    public class ImportModel : IValidatableObject
+    {
+        [Required(ErrorMessage = "You must choose a file.")]
+        public HttpPostedFileBase Database { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet".Equals(Database.ContentType))
+                yield return new ValidationResult("You must choose an XLSX file.", new string[] { "Database" });
+        }
+    }
+}
